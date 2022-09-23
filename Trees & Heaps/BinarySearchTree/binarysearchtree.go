4424174e1622e4d7c11ac3d7ef15c1e3c59ae6b3
albertopformoso <albertopformoso@gmail.com>
2022-09-23 13:23:57 -0500

    Binary Search Tree improved

--- Trees & Heaps/BinarySearchTree/binarysearchtree.go
+++ Trees & Heaps/BinarySearchTree/binarysearchtree.go
@@ -1,64 +1,138 @@
 package main
 
-import "log"
-
-var count int
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 // Node :represents the components of a binary search tree
 type Node struct {
-	Key   int
+	Value int
 	Left  *Node
 	Right *Node
 }
 
+// BST : represents the binary search tree structure
+type BST struct {
+	Root *Node
+	Len  int
+}
+
+func (n Node) String() string {
+	return strconv.Itoa(n.Value)
+}
+
+func (b BST) String() string {
+	sb := strings.Builder{}
+	b.inOrderTraversal(&sb)
+	return sb.String()
+}
+
+func (b BST) inOrderTraversal(sb *strings.Builder) {
+	b.inOrderTraversalByNode(sb, b.Root)
+}
+
+func (b BST) inOrderTraversalByNode(sb *strings.Builder, root *Node) {
+	if root == nil {
+		return
+	}
+
+	b.inOrderTraversalByNode(sb, root.Left)
+	sb.WriteString(fmt.Sprintf("%s ", root))
+	b.inOrderTraversalByNode(sb, root.Right)
+}
+
 // Insert : will add anode to the tree
 // the key to add should not be already in the tree
-func (n *Node) Insert(k int) {
-	if n.Key < k {
-		// move to the right
-		if n.Right == nil {
-			n.Right = &Node{Key: k}
-		} else {
-			n.Right.Insert(k)
-		}
-	} else if n.Key > k {
-		// move to the left
-		if n.Left == nil {
-			n.Left = &Node{Key: k}
-		} else {
-			n.Left.Insert(k)
-		}
+func (b *BST) Insert(value int) {
+	b.Root = b.insertByNode(b.Root, value)
+	b.Len++
+}
+
+func (b *BST) insertByNode(root *Node, value int) *Node {
+	if root == nil {
+		return &Node{Value: value}
 	}
+
+	if value < root.Value {
+		root.Left = b.insertByNode(root.Left, value)
+	} else {
+		root.Right = b.insertByNode(root.Right, value)
+	}
+
+	return root
 }
 
 // Search : will take in a key value
 // and return true if there is a node with that value
-func (n *Node) Search(k int) bool {
-	count++
-
-	if n == nil {
-		return false
-	}
-
-	if n.Key < k {
-		// move right
-		return n.Right.Search(k)
-	} else if n.Key > k {
-		// move left
-		return n.Left.Search(k)
-	}
-
-	return true
+func (b *BST) Search(value int) (*Node, bool) {
+	return b.searchByNode(b.Root, value)
 }
 
+func (b *BST) searchByNode(root *Node, value int) (*Node, bool) {
+	if root == nil {
+		return nil, false
+	}
+
+	if value == root.Value {
+		return root, true
+	} else if value < root.Value {
+		return b.searchByNode(root.Left, value)
+	} else {
+		return b.searchByNode(root.Right, value)
+	}
+}
+
+// Remove : will take a key value
+// and delete it from the BST
+func (b *BST) Remove(value int) {
+	b.removeByNode(b.Root, value)
+}
+
+func (b *BST) removeByNode(root *Node, value int) *Node {
+	if root == nil {
+		return root
+	}
+
+	if value > root.Value {
+		root.Right = b.removeByNode(root.Right, value)
+	} else if value < root.Value {
+		root.Left = b.removeByNode(root.Left, value)
+	} else {
+		if root.Left == nil {
+			return root.Right
+		} else {
+			tmp := root.Left
+			for tmp.Right != nil {
+				tmp = tmp.Right
+			}
+
+			root.Value = tmp.Value
+			root.Left = b.removeByNode(root.Left, value)
+		}
+	}
+
+	return root
+}
 
 func main() {
-	tree := &Node{Key: 100}
-	tree.Insert(200)
-	tree.Insert(300)
-	tree.Insert(50)
-	log.Println(tree)
-	tree.Search(300)
-	log.Println(tree.Search(300))
-	log.Println(count)
+	n := &Node{1, nil, nil}
+	n.Left = &Node{0, nil, nil}
+	n.Right = &Node{2, nil, nil}
+	b := BST{n, 3}
+	fmt.Println(b)
+
+	b.Insert(4)
+	b.Insert(5)
+	b.Insert(6)
+	fmt.Println(b)
+
+	fmt.Println(b.Search(6))
+	fmt.Println(b.Search(3))
+
+	fmt.Printf("\n%v\n", b)
+	b.Remove(6)
+	fmt.Println(b)
+	fmt.Println(b.Search(6))
 }
